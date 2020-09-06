dd5d3386a342cdf20baafda00dd05c004cab12f0
alicelandroe <alice@landroe.com>
2020-09-06 13:11:26 +0200

    hamburger acn open and close

--- public/index.js
+++ public/index.js
@@ -1,5 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
+const openHamburger = document.querySelector('.open-hamburger');
+const closeHamburger = document.querySelector('.close-hamburger');
 
 hamburger.addEventListener('click', () => {
-
+    openHamburger.style.display = 'block';
 });
+
+closeHamburger.addEventListener('click', () => {
+    openHamburger.style.display = 'none';
+});
+
+
