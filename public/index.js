5ef099b04faf04972a87ce6b96786ae13c347239
alicelandroe <alice@landroe.com>
2020-09-06 14:39:35 +0200

    refractoring hamburger animation code

--- public/index.js
+++ public/index.js
@@ -1,17 +1,28 @@
+// HAMBURGER MENU 
 const hamburger = document.querySelector('.hamburger');
 const openHamburger = document.querySelector('.open-hamburger');
 
+function openHamburgerMenu() {
+    openHamburger.style.display = 'block';
+    hamburger.classList.add('is-active');
+}
 
-hamburger.addEventListener('click', () => {
-    if (openHamburger.style.display === '') {
-        openHamburger.style.display = 'block';
-        hamburger.classList.add('is-active');
+function closeHamburgerMenu() {
+    openHamburger.style.display = '';
+    hamburger.classList.remove('is-active');
+}
+
+function toggleHamburgerMenu() {
+    const isClosed = openHamburger.style.display === '';
+
+    if (isClosed) {
+        openHamburgerMenu();
     } else {
-        openHamburger.style.display = '';
-        hamburger.classList.remove('is-active');
+        closeHamburgerMenu();
     }
-});
+}
 
-
+hamburger.addEventListener('click', toggleHamburgerMenu); 
+// END: HAMBURGER MENU
 
 
