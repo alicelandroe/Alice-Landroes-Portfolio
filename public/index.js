8309b1f0d6e4baae0f4aafc565f0dd28e85fc554
Erlend <elandroe@icloud.com>
2020-09-09 18:15:01 +0200

    Fix overlay

--- public/index.js
+++ public/index.js
@@ -1,15 +1,18 @@
 // HAMBURGER MENU 
 const hamburger = document.querySelector('.hamburger');
 const openHamburger = document.querySelector('.open-hamburger');
+const grayOverlay = document.querySelector('.gray-overlay');
 
 function openHamburgerMenu() {
     openHamburger.style.left = '0vw';
     hamburger.classList.add('is-active');
+    grayOverlay.classList.add('gray-overlay--show');
 }
 
 function closeHamburgerMenu() {
     openHamburger.style.left = '-100vw';
     hamburger.classList.remove('is-active');
+    grayOverlay.classList.remove('gray-overlay--show');
 }
 
 function toggleHamburgerMenu() {
